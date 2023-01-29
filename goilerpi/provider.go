ee853700faf12545b28ca67e534954c94145e654
Felipe T. De Carli <felipetdecarli@gmail.com>
2023-01-29 20:42:26 -0300

    feat: add provider.go config

---
+++ goilerpi/provider.go
@@ -0,0 +1,14 @@
+package goilerpi
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{
+			"goilerpi_users": dataSourceUsers(),
+		},
+	}
+}
