16eec063b24930a5b6ed6cc3ea79da6758baec3d
Felipe T. De Carli <felipetdecarli@gmail.com>
2023-01-29 20:39:04 -0300

    feat: add data_source for user type

---
+++ goilerpi/data_source_user.go
@@ -0,0 +1,92 @@
+package hashicups
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceUsersRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	var diags diag.Diagnostics
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/user", "http://localhost:4000"), nil)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	r, err := client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	defer r.Body.Close()
+
+	users := make([]map[string]interface{}, 0)
+	err = json.NewDecoder(r.Body).Decode(&users)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("users", users); err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+
+	return diags
+}
+
+func dataSourceCoffees() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceUsersRead,
+		Schema: map[string]*schema.Schema{
+			"users": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"age": &schema.Schema{
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"address": &schema.Schema{
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"state": &schema.Schema{
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"city": &schema.Schema{
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"pincode": &schema.Schema{
+										Type:     schema.TypeInt,
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
